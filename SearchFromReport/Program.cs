c12d1a0ed236db26b7fdf5fbf3748dc3aba492ff
Mahesh <contact@maheshyadav.com.np>
2024-02-14 15:57:19 +0545

    Renamed main form

--- SearchFromReport/Program.cs
+++ SearchFromReport/Program.cs
@@ -16,7 +16,7 @@ namespace SearchFromReport
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
